2d8e861a8e3e01c96e2621b495b241ab6211fd46
CarlosDv93 <carlos9_cv@hotmail.com>
2020-08-19 19:25:51 -0300

    Primeira parte do vídeo

---
+++ src/main/java/com/example/demo/dao/PersonDataAccessService.java
@@ -0,0 +1,41 @@
+package com.example.demo.dao;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.Person;
+
+@Repository("postgres")
+public class PersonDataAccessService implements PersonDao{
+
+	@Override
+	public int insertPerson(UUID id, Person person) {
+		return 0;
+	}
+
+	@Override
+	public List<Person> selectAllPeople() {
+		return Arrays.asList((new Person(UUID.randomUUID(), "FROM POSTGRESS DB")));
+	}
+
+	@Override
+	public Optional<Person> selectPersonById(UUID id) {
+		return Optional.empty();
+	}
+
+	@Override
+	public int deletePersonById(UUID id) {
+		return 0;
+	}
+
+	@Override
+	public int updatePersonById(UUID id, Person person) {
+		return 0;
+	}
+	
+
+}
